7a6eedd5b5ea8e658961794f68bda157b46e0b66
VisualViolet <savannah_b_miller@outlook.com>
2022-12-21 19:31:35 -0800

    completed script.js and updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# password-generator
+# Password Generator
+
+## Description
+
+This password generator asks for user input on password parameters and generates a random password that fulfills their selections. I have utilized new JavaScript concepts I have learned in the UW COding Bootcamp, such as functions, arrays, if statements, and prompts to create a working application complete with error handling. Thinking of a random and unique password can be difficult, so why not let an application do all the work for us! In creating this password generator, I have learned how to use basic JavaScript concepts to obtain user input and display interactive feedback!
+
+![webpage start](./assets/images/pass-gen-1.jpg)
+
+![webpage prompt](./assets/images/pass-gen-2.jpg)
+
+![webpage followup prompt](./assets/images/pass-gen-3.jpg)
+
+![generated password displayed on page](./assets/images/pass-gen-4.jpg)
+
+## Installation
+
+N/A. No installation necessary! Simply view my deployed page to test it out for yourself.
+
+## Usage
+
+This application can be used to generate a random password between 8 - 128 characters with your choice of character types: uppercase, lowercase, numeric, or special. Simply follow the prompts and your password will generate! Students and new developers may also find it beneficial to review the code used to create this application as many of the basic JavaScript concepts are used. The code has been commentated to explain it's functionality. Feel free to view it as you use the password generator and follow along with what's happening in the background!
+
+## Credits
+
+N/A
+
+## License
+
+MIT License
+Copyright (c) 2022 VisualViolet
+
+Please view the license in the repo for more information.
--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Global Variables
+const characterTypes = ["lowercase", "uppercase", "numeric", "special"];
+var selectedCharacterTypes = [];
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numChar = "0123456789";
+const specialChar = "!@#$%^&()_-+={[}]|\/:'<,>.?~";
+var characterCombination = "";
+var passwordLength;
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -7,8 +17,86 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Prompt user for password length
+function getPasswordLength() {
+  selectedCharacterTypes = [];
+  passwordLength = prompt("How long would your like your password to be? Pick a number between 8 and 128.");
+  if (passwordLength != null && (passwordLength < 8 || passwordLength > 128)) // allows user to cancel with null, but empty string and other parameters specificied not accepted. 
+  {                                                                           // OK = "", Cancel = null
+    alert("Please choose a number between 8 and 128.");
+    getPasswordLength();
+  } 
+  else if (passwordLength == null) // allows user to exit
+  {
+    return;
+  }
+  return passwordLength;
+}
+
+// Prompt user for character types
+function getCharacterTypes() {
+  var passChar = prompt("Please select at least one from the following character types: lowercase, uppercase, numeric, special. Click OK after each selection and Cancel when done.");
+  if (passChar == null) // allows user to exit
+  {
+    return;
+  }
+  else if (characterTypes.includes(passChar.toLowerCase())) // checks if user input matches valid inputs in array and converts to lowercase. Adds to selected types array.
+  {
+    selectedCharacterTypes.push(passChar.toLowerCase());
+    getCharacterTypes();
+  }
+  else // error handling
+  {
+    alert("Invalid character choice. Please check spelling and try again.");
+    getCharacterTypes();
+  }
+}
+
+// Removes duplicates from array by converting to a set
+function clearDuplicate(dirtyArray) {
+  var cleanSet = new Set(dirtyArray);
+  return cleanSet;
+}
+
+// Creates character combination string with user selected characters
+function createCharacterCombination(userSet){
+  characterCombination = "";
+  for (i = 0; i < userSet.length; i++) {
+    if (userSet[i] == "uppercase") 
+    {
+      characterCombination = characterCombination + upperCase;
+    }
+    else if (userSet[i] == "lowercase")
+    {
+      characterCombination = characterCombination + lowerCase;
+    }
+    else if (userSet[i] == "numeric")
+    {
+      characterCombination = characterCombination + numChar;
+    }
+    else if (userSet[i] == "special")
+    {
+      characterCombination = characterCombination + specialChar;
+    }
+  }
+}
+
+// Randomly generates password using function return values and a for loop
+function generatePassword() {
+  passwordLength = getPasswordLength();
+  getCharacterTypes();
+  var cleanPicks = Array.from(clearDuplicate(selectedCharacterTypes));
+  createCharacterCombination(cleanPicks);
+  var genPass = "";
+
+  for (var i = 0; i < passwordLength; i++) 
+  {
+    genPass += characterCombination.charAt(Math.floor(Math.random() * characterCombination.length)); // selects random index ID's from characterCombination array
+  }
+  return genPass;
+}
